f482342b0d7b5c7d38a313fbfc852bbca984f5f8
Grant Wu <grantwu2@gmail.com>
2017-09-04 18:52:30 -0700

    Convert Sphere-related functions to Sphere instance methods
    Starting to see why graphics/game programming skews heavily towards OO

--- raytracer.js
+++ raytracer.js
@@ -53,7 +53,7 @@ class RayTracer {
     // INTERSECTION?
     const t = this.scene.objects.reduce((accumT, sphere) => {
       // Get closest intersection of the cast ray with a sphere
-      const newT = intersectRaySphere(ray, sphere);
+      const newT = sphere.intersectRay(ray);
 
       if (newT >= 0 && newT < accumT) {
         // New smallest valid intersection
@@ -70,7 +70,7 @@ class RayTracer {
         ray.origin,
         vScale(ray.direction, t)
       );
-      const surfaceNormal = getSurfaceNormal(intersectedSphere, intersectPoint);
+      const surfaceNormal = intersectedSphere.getSurfaceNormal(intersectPoint);
 
       // REFLECTIONS
       if (numBounces > 0) {
@@ -106,7 +106,7 @@ class RayTracer {
         );
 
         for (let i = 0; i < this.scene.objects.length; i++) {
-          const shadowT = intersectRaySphere(shadowRay, this.scene.objects[i]);
+          const shadowT = this.scene.objects[i].intersectRay(shadowRay);
           if (0.00001 < shadowT && shadowT < 1) {
             // In shadow, don't add diffuse / specular
             return;
@@ -156,58 +156,3 @@ class RayTracer {
     return color;
   }
 }
-
-// Find smallest scalar t such that o + td
-// intersects with the given Sphere, where o
-// and d are the Vectors that represent the origin
-// and direction of the Ray, respectively.
-// Returns -1 if no intersection.
-// 0 is valid, as that means that the intersection point
-// lies directly on the image plane (b/c the origin of the Ray
-// is p, see above)
-function intersectRaySphere(ray, sphere) {  // (Ray, Sphere) -> int
-  // at**2 + bt + c = 0
-
-  // a, (len(d)**2), squared length of ray's direction
-  const a = vDotProduct(ray.direction, ray.direction);
-
-  // b, 2<c', d>, 2 times dot product of vector from ray
-  // origin to sphere center and ray direction
-  const cPrime = vSub(ray.origin, sphere.center);
-  const b = 2 * vDotProduct(cPrime, ray.direction);
-
-  // c, len(c')**2 - r**2, squared length of vector from
-  // ray origin to sphere center - radius squared
-  const c = vDotProduct(cPrime, cPrime) - Math.pow(sphere.radius, 2);
-
-  // Discriminant, b**2 - 4ac
-  const discriminant = Math.pow(b, 2) - (4*a*c);
-
-  if (discriminant < 0) {
-    // t can't be real number
-    return -1;
-  }
-
-  // t is negative or positive
-  // Apply quadratic formula:
-  // t = (-b +- sqrt(d)) / 2a
-  const tPlus = (-b + Math.sqrt(discriminant)) / (2 * a);
-  const tMinus = (-b - Math.sqrt(discriminant)) / (2 * a);
-
-  if (tPlus < 0 && tMinus < 0) {
-    // Both intersections are before image plane
-    return -1;
-  }
-
-  // At least one t is positive
-  // We want to return the smallest positive one, as
-  // that represents the visible intersection, of which
-  // there may be 0, 1, or 2 with spheres, which mean
-  // no intersection, 1 intersection (tangent), and 2
-  // intersections, respectively
-  return Math.min(
-    Math.max(tPlus, 0),
-    Math.max(tMinus, 0)
-  );
-}
-
--- sphere.js
+++ sphere.js
@@ -5,9 +5,63 @@ class Sphere {
         this.radius = radius;
         this.material = material;
     }
-}
 
-function getSurfaceNormal(sphere, point) {  // (Sphere, Vector) -> Vector (unit)
-    const direction = vSub(point, sphere.center);
-    return vScale(direction, 1 / vLength(direction));
+    // Find smallest scalar t such that o + td
+    // intersects with the given Sphere, where o
+    // and d are the Vectors that represent the origin
+    // and direction of the Ray, respectively.
+    // Returns -1 if no intersection.
+    // 0 is valid, as that means that the intersection point
+    // lies directly on the image plane (b/c the origin of the Ray
+    // is p, see above)
+    intersectRay(ray) {  // (Ray) -> int
+        // at**2 + bt + c = 0
+
+        // a, (len(d)**2), squared length of ray's direction
+        const a = vDotProduct(ray.direction, ray.direction);
+
+        // b, 2<c', d>, 2 times dot product of vector from ray
+        // origin to sphere center and ray direction
+        const cPrime = vSub(ray.origin, this.center);
+        const b = 2 * vDotProduct(cPrime, ray.direction);
+
+        // c, len(c')**2 - r**2, squared length of vector from
+        // ray origin to sphere center - radius squared
+        const c = vDotProduct(cPrime, cPrime) - Math.pow(this.radius, 2);
+
+        // Discriminant, b**2 - 4ac
+        const discriminant = Math.pow(b, 2) - (4 * a * c);
+
+        if (discriminant < 0) {
+            // t can't be real number
+            return -1;
+        }
+
+        // t is negative or positive
+        // Apply quadratic formula:
+        // t = (-b +- sqrt(d)) / 2a
+        const tPlus = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const tMinus = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        if (tPlus < 0 && tMinus < 0) {
+            // Both intersections are before image plane
+            return -1;
+        }
+
+        // At least one t is positive
+        // We want to return the smallest positive one, as
+        // that represents the visible intersection, of which
+        // there may be 0, 1, or 2 with spheres, which mean
+        // no intersection, 1 intersection (tangent), and 2
+        // intersections, respectively
+        return Math.min(
+            Math.max(tPlus, 0),
+            Math.max(tMinus, 0)
+        );
+    }
+
+    getSurfaceNormal(point) {  // (Vector) -> Vector (unit)
+        const direction = vSub(point, this.center);
+        return vScale(direction, 1 / vLength(direction));
+    }
 }
